c9e7edd74b690252ff97cdece4aef120ab78d78a
Aucante <sebastien.aucante@gmail.com>
2024-03-11 21:00:37 +0100

    homepage chart

--- src/app/demo/components/homepage/homepage.component.ts
+++ src/app/demo/components/homepage/homepage.component.ts
@@ -1,13 +1,77 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from "rxjs";
+import { MenuItem } from "primeng/api";
 
 @Component({
     templateUrl: './homepage.component.html'
 })
 export class HomepageComponent implements OnInit, OnDestroy{
 
+    items!: MenuItem[];
+
+    chartData: any;
+
+    chartOptions: any;
+
     subscription!: Subscription;
     ngOnInit() {
+        this.initChart();
+
+        this.items = [
+            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
+            { label: 'Remove', icon: 'pi pi-fw pi-minus' }
+        ];
+    }
+
+    initChart() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+        this.chartData = {
+            labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            datasets: [
+                {
+                    label: 'BTC',
+                    data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    backgroundColor: documentStyle.getPropertyValue('--green-600'),
+                    borderColor: documentStyle.getPropertyValue('--green-600'),
+                    tension: .1
+                }
+            ]
+        };
+
+        this.chartOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
     }
 
     ngOnDestroy() {
