06245b9178022bc4dc2f0a766ee7346e92dbee40
Aucante <sebastien.aucante@gmail.com>
2024-03-20 21:49:22 +0100

    Add assets section by portfolio

--- src/app/demo/components/portfolio/portfolio.component.ts
+++ src/app/demo/components/portfolio/portfolio.component.ts
@@ -52,6 +52,8 @@ export class PortfolioComponent implements OnInit, OnDestroy{
             this.chartOptions = ChartHelper.initChart(chartLabels, chartDatas)[0];
             this.chartData = ChartHelper.initChart(chartLabels, chartDatas)[1];
 
+            console.table(this.portfolio.portfolioLines)
+
         });
 
 
