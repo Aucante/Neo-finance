220f742902c992d3757b2c474f41b490fced54ce
Aucante <sebastien.aucante@gmail.com>
2024-03-21 21:49:44 +0100

    Dark mode

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -17,4 +17,35 @@ export class AppTopBarComponent {
     @ViewChild('topbarmenu') menu!: ElementRef;
 
     constructor(public layoutService: LayoutService) { }
+
+    changeTheme() {
+        console.log(this.theme)
+        const theme = this.theme === "vela-green" ? "md-light-deeppurple" : "vela-green";
+        this.layoutService.config.update((config) => ({
+            ...config,
+            theme: theme,
+        }));
+    }
+
+    changeColorScheme() {
+        console.log(this.colorScheme)
+        const colorScheme = this.colorScheme === "dark" ? "light" : "dark";
+        this.layoutService.config.update((config) => ({
+            ...config,
+            colorScheme: colorScheme,
+        }));
+    }
+
+    toggleDarkMode() {
+        this.changeTheme();
+        this.changeColorScheme();
+    }
+
+    get theme(): string {
+        return this.layoutService.config().theme;
+    }
+
+    get colorScheme(): string {
+        return this.layoutService.config().colorScheme;
+    }
 }
