7c7440af02dc8b4df0b45147f4bc3da29789b55a
Aucante <sebastien.aucante@gmail.com>
2024-04-01 22:09:35 +0200

    Mock Asset

--- src/app/demo/service/asset.service.ts
+++ src/app/demo/service/asset.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
+
 import { Asset } from "../api/asset";
 import { MOCK_ASSETS } from "./mock/asset-mock";
 
 @Injectable()
 export class AssetService {
-
     getAssets(): Asset[] {
         return MOCK_ASSETS;
     }
