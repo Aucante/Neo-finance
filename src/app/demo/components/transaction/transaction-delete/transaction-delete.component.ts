74a6c8b8277fb04056f7ac91d06ba250ac70574c
Aucante <sebastien.aucante@gmail.com>
2024-06-05 21:25:20 +0200

    Transaction delete component

--- src/app/demo/components/transaction/transaction-delete/transaction-delete.component.ts
+++ src/app/demo/components/transaction/transaction-delete/transaction-delete.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Portfolio, PortfolioService } from "../../../service/portfolio.service";
 import { ButtonModule } from "primeng/button";
 import { DropdownModule } from "primeng/dropdown";
 import { InputTextModule } from "primeng/inputtext";
